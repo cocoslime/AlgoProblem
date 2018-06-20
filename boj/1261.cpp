d5b76fdfb0642c26d1c350fff636cbb4ea0193ef
dongmin <dongmin.kim@pusan.ac.kr>
2018-06-20 19:03:44 +0900

    solve 1753

---
+++ boj/1261.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Dong's MacBook Pro on 2018. 6. 11..
+//
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <utility>
+#include <climits>
+#include <queue>
+#include <functional>
+#include <stack>
+#include <map>
+#include <cassert>
+
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define INF 10000000
+
+int map[101][101];
+int painted_map[101][101];
+
+int main(){
+    return 0;
+
+}
---
+++ boj/1753.cpp
@@ -0,0 +1,87 @@
+//
+// Created by Dong's MacBook Pro on 2018. 6. 11..
+//
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <utility>
+#include <climits>
+#include <queue>
+#include <functional>
+#include <stack>
+#include <map>
+#include <cassert>
+
+using namespace std;
+
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define INF 10000000
+
+#define MAXV 20000
+
+int S;
+int V, E;
+int dist[MAXV];
+
+vector<vii> adjList;
+
+void dijkstra(){
+    bool checked[MAXV] = {0};
+    priority_queue<ii> pq;
+    pq.push(make_pair(0, S));
+    dist[S] = 0;
+    checked[S] = true;
+    while (!pq.empty()){
+        int src = pq.top().second;
+        pq.pop();
+        checked[src] = true;
+        for (auto pi : adjList[src]){
+            int dest = pi.second;
+            int w = pi.first;
+            if (checked[dest]) continue;
+            if (dist[src] + w < dist[dest]){
+                dist[dest] = dist[src] + w;
+                pq.push(make_pair(-(dist[src] + w), dest));
+            }
+        }
+    }
+}
+
+int main(){
+    cin >> V >> E;
+    int start;
+    cin >> start;
+
+    S = start - 1;
+
+    adjList.assign(V, vii());
+
+    std::fill(dist, dist + MAXV, INF);
+
+    for (int i = 0 ; i < E ; i++){
+        int src, dest, w;
+        cin >> src >> dest >> w;
+        adjList[src-1].emplace_back(w, dest - 1);
+    }
+
+     dijkstra();
+
+    for (int i = 0 ; i < V ; i++){
+        if (dist[i] == INF)
+            printf("INF\n");
+        else
+            printf("%d\n", dist[i]);
+    }
+    return 0;
+}
