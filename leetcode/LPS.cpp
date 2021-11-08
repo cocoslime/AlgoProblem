baa689b479b80462a3acfd29f462a475b31ffa94
Dongmin <cocoslime@nsr.re.kr>
2021-11-08 20:22:35 +0900

    LPS solved

---
+++ leetcode/LPS.cpp
@@ -0,0 +1,67 @@
+//
+// Created by Dongmin on 2021/11/08.
+//
+
+//// https://leetcode.com/problems/longest-palindromic-substring/
+
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <unordered_map>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <set>
+#include <vector>
+#include <utility>
+#include <climits>
+#include <queue>
+#include <functional>
+#include <stack>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <sstream>
+#include <numeric>
+
+using namespace std;
+
+bool isPalindrome(string& s, int start, int end){
+    for (int i = start, j = end - 1 ; i < j ; i ++, j--){
+        if (s[i] != s[j]){
+            return false;
+        }
+    }
+    return true;
+}
+
+string longestPalindrome(string s) {
+    if (s.length() == 1)
+        return s;
+
+    for (int size = s.length() ; size >= 2 ; size--){
+        for (int i = 0 ; i <= s.length() - size ; i++){
+            int j = i + size;
+            if (isPalindrome(s, i, j)){
+                return s.substr(i, j - i);;
+            }
+        }
+    }
+
+    return s.substr(0, 1);
+}
+
+int main(){
+    std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+//    cout << longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheeart") << endl;
+//    cout << longestPalindrome("kztakrekvefgchersuoiuatzlmwynzjhdqqftjcqmntoyckqfawikkdrnfgbwtdpbkymvwoumurjdzygyzsbmwzpcxcdmmpwzmeibligwiiqbecxwyxigikoewwrczkanwwqukszsbjukzumzladrvjefpegyicsgctdvldetuegxwihdtitqrdmygdrsweahfrepdcudvyvrggbkthztxwicyzazjyeztytwiyybqdsczozvtegodacdokczfmwqfmyuixbeeqluqcqwxpyrkpfcdosttzooykpvdykfxulttvvwnzftndvhsvpgrgdzsvfxdtzztdiswgwxzvbpsjlizlfrlgvlnwbjwbujafjaedivvgnbgwcdbzbdbprqrflfhahsvlcekeyqueyxjfetkxpapbeejoxwxlgepmxzowldsmqllpzeymakcshfzkvyykwljeltutdmrhxcbzizihzinywggzjctzasvefcxmhnusdvlderconvaisaetcdldeveeemhugipfzbhrwidcjpfrumshbdofchpgcsbkvaexfmenpsuodatxjavoszcitjewflejjmsuvyuyrkumednsfkbgvbqxfphfqeqozcnabmtedffvzwbgbzbfydiyaevoqtfmzxaujdydtjftapkpdhnbmrylcibzuqqynvnsihmyxdcrfftkuoymzoxpnashaderlosnkxbhamkkxfhwjsyehkmblhppbyspmcwuoguptliashefdklokjpggfiixozsrlwmeksmzdcvipgkwxwynzsvxnqtchgwwadqybkguscfyrbyxudzrxacoplmcqcsmkraimfwbauvytkxdnglwfuvehpxd") << endl;
+
+    std::chrono::duration<double> sec = std::chrono::system_clock::now() - start;
+    cout << "Seconds : " << sec.count() << "s" << endl;
+    cout << longestPalindrome("aacabdkacaa") << endl;
+//    cout << longestPalindrome("abcba") << endl;
+    return 0;
+}
