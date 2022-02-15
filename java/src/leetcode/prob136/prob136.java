b44f3474e4698c378a1d9fac5308ca10bea84c97
Dongmin <dongmen94@gmail.com>
2022-02-15 19:01:10 +0900

    prob 136

---
+++ java/src/leetcode/prob136/prob136.java
@@ -0,0 +1,17 @@
+package leetcode.prob136;
+
+import java.util.*;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0 ; i < nums.length - 1 ; i += 2) {
+            if (nums[i] != nums[i+1])
+                return nums[i];
+        }
+        return nums[nums.length - 1];
+    }
+
+    public static void main(String[] args) {
+    }
+}
