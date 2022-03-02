3944f658d38a770a05ff01efc70a6e3fbb0e9f14
Dongmin <dongmen94@gmail.com>
2022-03-02 20:33:48 +0900

    prob392 kt

---
+++ java/src/leetcode/prob392/prob392.kt
@@ -0,0 +1,14 @@
+package leetcode.prob392
+
+class Solution {
+    fun isSubsequence(s: String, t: String): Boolean {
+        var i = 0
+        var j = 0
+
+        while (i < s.length && j < t.length) {
+            if (s[i] == t[j++]) i++
+        }
+
+        return (i == s.length)
+    }
+}
